16467e1fb2df4ac1ece86ff82275099733285823
Jahid Rahman <rahmanjahid645@gmail.com>
2026-08-08 15:04:09 +0600

    JWT for Authentication

---
+++ app/Helper/JWTToken.php
@@ -0,0 +1,36 @@
+<?php 
+namespace App\Helper;
+
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken{
+
+    function CreateToken($userEmail):string{
+        $key = env('JWT_KEY');
+        $payload = [
+            'iss' => 'laravel-token', // Issuer
+            'iat' => time(), // Issued at
+            'exp' => time() + 3600, // Expiration time (1 hour)
+            'userEmail' => $userEmail, // User email
+        ];
+
+        return JWT::encode($payload, $key, 'HS256');
+    }
+
+    function VerifyToken($token){
+        try {
+
+            $key = env('JWT_KEY');
+            $decode = JWT::decode($token, new Key($key, 'HS256'));
+            return $decode->userEmail;
+            } 
+
+        catch (Exception $e) {
+            return 'Invalid Token';
+        }
+    }
+
+
+}
